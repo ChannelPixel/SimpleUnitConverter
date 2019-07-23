69e951814426a8722ad7d7d3e19bb26eb1057801
DanielCherrin <45531810+ChannelPixel@users.noreply.github.com>
2019-07-23 17:36:06 +0800

    Initial commit

---
+++ danielCherrinAssignment01/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace danielCherrinAssignment01
+{
+    partial class frm_distanceConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.lbl_LABEL_INPUT = new System.Windows.Forms.Label();
+            this.lbl_inputLabel = new System.Windows.Forms.Label();
+            this.lbl_LABEL_EQUALS = new System.Windows.Forms.Label();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.lbl_outputLabel = new System.Windows.Forms.Label();
+            this.btn_inchesToCms = new System.Windows.Forms.Button();
+            this.btn_FeetToMetres = new System.Windows.Forms.Button();
+            this.btn_YardsToMetres = new System.Windows.Forms.Button();
+            this.btn_MilesToKms = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_LABEL_TYPE = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_input
+            // 
+            this.txt_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txt_input.Location = new System.Drawing.Point(24, 35);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(109, 32);
+            this.txt_input.TabIndex = 0;
+            this.txt_input.Click += new System.EventHandler(this.txt_input_Click);
+            this.txt_input.TextChanged += new System.EventHandler(this.txt_input_TextChanged);
+            this.txt_input.Enter += new System.EventHandler(this.txt_input_Enter);
+            // 
+            // lbl_LABEL_INPUT
+            // 
+            this.lbl_LABEL_INPUT.AutoSize = true;
+            this.lbl_LABEL_INPUT.Location = new System.Drawing.Point(21, 13);
+            this.lbl_LABEL_INPUT.Name = "lbl_LABEL_INPUT";
+            this.lbl_LABEL_INPUT.Size = new System.Drawing.Size(157, 13);
+            this.lbl_LABEL_INPUT.TabIndex = 1;
+            this.lbl_LABEL_INPUT.Text = "Enter the value to be converted";
+            this.lbl_LABEL_INPUT.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_inputLabel
+            // 
+            this.lbl_inputLabel.AutoSize = true;
+            this.lbl_inputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbl_inputLabel.Location = new System.Drawing.Point(139, 42);
+            this.lbl_inputLabel.Name = "lbl_inputLabel";
+            this.lbl_inputLabel.Size = new System.Drawing.Size(51, 20);
+            this.lbl_inputLabel.TabIndex = 2;
+            this.lbl_inputLabel.Text = "label2";
+            this.lbl_inputLabel.Visible = false;
+            // 
+            // lbl_LABEL_EQUALS
+            // 
+            this.lbl_LABEL_EQUALS.AutoSize = true;
+            this.lbl_LABEL_EQUALS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_LABEL_EQUALS.Location = new System.Drawing.Point(213, 40);
+            this.lbl_LABEL_EQUALS.Name = "lbl_LABEL_EQUALS";
+            this.lbl_LABEL_EQUALS.Size = new System.Drawing.Size(25, 25);
+            this.lbl_LABEL_EQUALS.TabIndex = 3;
+            this.lbl_LABEL_EQUALS.Text = "=";
+            this.lbl_LABEL_EQUALS.Visible = false;
+            // 
+            // txt_output
+            // 
+            this.txt_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txt_output.Location = new System.Drawing.Point(258, 35);
+            this.txt_output.Name = "txt_output";
+            this.txt_output.ReadOnly = true;
+            this.txt_output.Size = new System.Drawing.Size(133, 32);
+            this.txt_output.TabIndex = 4;
+            this.txt_output.Visible = false;
+            // 
+            // lbl_outputLabel
+            // 
+            this.lbl_outputLabel.AutoSize = true;
+            this.lbl_outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbl_outputLabel.Location = new System.Drawing.Point(397, 42);
+            this.lbl_outputLabel.Name = "lbl_outputLabel";
+            this.lbl_outputLabel.Size = new System.Drawing.Size(51, 20);
+            this.lbl_outputLabel.TabIndex = 5;
+            this.lbl_outputLabel.Text = "label4";
+            this.lbl_outputLabel.Visible = false;
+            // 
+            // btn_inchesToCms
+            // 
+            this.btn_inchesToCms.Location = new System.Drawing.Point(24, 120);
+            this.btn_inchesToCms.Name = "btn_inchesToCms";
+            this.btn_inchesToCms.Size = new System.Drawing.Size(165, 23);
+            this.btn_inchesToCms.TabIndex = 6;
+            this.btn_inchesToCms.Text = "&Inches to Centimetres";
+            this.btn_inchesToCms.UseVisualStyleBackColor = true;
+            this.btn_inchesToCms.Click += new System.EventHandler(this.btn_inchesToCms_Click);
+            // 
+            // btn_FeetToMetres
+            // 
+            this.btn_FeetToMetres.Location = new System.Drawing.Point(24, 149);
+            this.btn_FeetToMetres.Name = "btn_FeetToMetres";
+            this.btn_FeetToMetres.Size = new System.Drawing.Size(165, 23);
+            this.btn_FeetToMetres.TabIndex = 7;
+            this.btn_FeetToMetres.Text = "&Feet to Metres";
+            this.btn_FeetToMetres.UseVisualStyleBackColor = true;
+            this.btn_FeetToMetres.Click += new System.EventHandler(this.btn_FeetToMetres_Click);
+            // 
+            // btn_YardsToMetres
+            // 
+            this.btn_YardsToMetres.Location = new System.Drawing.Point(25, 178);
+            this.btn_YardsToMetres.Name = "btn_YardsToMetres";
+            this.btn_YardsToMetres.Size = new System.Drawing.Size(165, 23);
+            this.btn_YardsToMetres.TabIndex = 8;
+            this.btn_YardsToMetres.Text = "&Yards to Metres";
+            this.btn_YardsToMetres.UseVisualStyleBackColor = true;
+            this.btn_YardsToMetres.Click += new System.EventHandler(this.btn_YardsToMetres_Click);
+            // 
+            // btn_MilesToKms
+            // 
+            this.btn_MilesToKms.Location = new System.Drawing.Point(25, 207);
+            this.btn_MilesToKms.Name = "btn_MilesToKms";
+            this.btn_MilesToKms.Size = new System.Drawing.Size(165, 23);
+            this.btn_MilesToKms.TabIndex = 9;
+            this.btn_MilesToKms.Text = "&Miles to Kilometres";
+            this.btn_MilesToKms.UseVisualStyleBackColor = true;
+            this.btn_MilesToKms.Click += new System.EventHandler(this.btn_MilesToKms_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(342, 207);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 10;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(423, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "or press \"esc\"";
+            // 
+            // lbl_LABEL_TYPE
+            // 
+            this.lbl_LABEL_TYPE.AutoSize = true;
+            this.lbl_LABEL_TYPE.Location = new System.Drawing.Point(22, 89);
+            this.lbl_LABEL_TYPE.Name = "lbl_LABEL_TYPE";
+            this.lbl_LABEL_TYPE.Size = new System.Drawing.Size(98, 13);
+            this.lbl_LABEL_TYPE.TabIndex = 12;
+            this.lbl_LABEL_TYPE.Text = "Type of conversion";
+            // 
+            // frm_distanceConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 254);
+            this.Controls.Add(this.lbl_LABEL_TYPE);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_MilesToKms);
+            this.Controls.Add(this.btn_YardsToMetres);
+            this.Controls.Add(this.btn_FeetToMetres);
+            this.Controls.Add(this.btn_inchesToCms);
+            this.Controls.Add(this.lbl_outputLabel);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.lbl_LABEL_EQUALS);
+            this.Controls.Add(this.lbl_inputLabel);
+            this.Controls.Add(this.lbl_LABEL_INPUT);
+            this.Controls.Add(this.txt_input);
+            this.KeyPreview = true;
+            this.Name = "frm_distanceConverter";
+            this.Text = "Distance Converter";
+            this.Load += new System.EventHandler(this.frm_distanceConverter_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frm_distanceConverter_KeyDown);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.frm_distanceConverter_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Label lbl_LABEL_INPUT;
+        private System.Windows.Forms.Label lbl_inputLabel;
+        private System.Windows.Forms.Label lbl_LABEL_EQUALS;
+        private System.Windows.Forms.TextBox txt_output;
+        private System.Windows.Forms.Label lbl_outputLabel;
+        private System.Windows.Forms.Button btn_inchesToCms;
+        private System.Windows.Forms.Button btn_FeetToMetres;
+        private System.Windows.Forms.Button btn_YardsToMetres;
+        private System.Windows.Forms.Button btn_MilesToKms;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_LABEL_TYPE;
+    }
+}
+
---
+++ danielCherrinAssignment01/Form1.cs
@@ -0,0 +1,212 @@
+﻿//Programmed by Daniel Robin Cherrin.
+//Email: daniel.r.cherrin@gmail.com
+//Latest revision: 15-Sep-2017.
+/*Purpose of the programm: Creates a form application that converts units of measurements into two txt boxes. one input, one output.
+  Have application when 'esc' key is pressed or a button control is pressed*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace danielCherrinAssignment01
+{
+    public partial class frm_distanceConverter : Form
+    {
+        public frm_distanceConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_inchesToCms_Click(object sender, EventArgs e)
+        {
+            const double dbl_CM_PER_INCH = 2.54;
+            double dbl_input;
+            double dbl_output;
+
+            //Input validation.
+            if (!double.TryParse(txt_input.Text, out dbl_input))
+            {
+                MessageBox.Show("Your entry is not a number or is empty - Please enter a positive number");
+                txt_input.Clear();
+                txt_input.Focus();
+            }
+            else if (dbl_input < 0)
+            {
+                MessageBox.Show("Your entry is a negative - Please enter a positive number");
+                txt_input.Clear();
+                txt_input.Focus();
+            }
+            //Validation successful. - Calculate inches to CMs
+            else
+            {
+                lbl_inputLabel.Text = "inches";
+                lbl_outputLabel.Text = "centimetres";
+                dbl_output = dbl_input * dbl_CM_PER_INCH;
+                txt_output.Text = dbl_output.ToString();
+                //Make outputs visible
+                lbl_LABEL_EQUALS.Visible = true;
+                txt_output.Visible = true;
+                lbl_outputLabel.Visible = true;
+                lbl_inputLabel.Visible = true;
+            }
+        }
+
+        private void btn_FeetToMetres_Click(object sender, EventArgs e)
+        {
+            const double dbl_FEET_PER_METRES = 0.3048;
+            double dbl_input;
+            double dbl_output;
+
+            //Input validation.
+            if (!double.TryParse(txt_input.Text, out dbl_input))
+            {
+                MessageBox.Show("Your entry is not a number or is empty - Please enter a positive number");
+                txt_input.Clear();
+                txt_input.Focus();
+            }
+            else if (dbl_input < 0)
+            {
+                MessageBox.Show("Your entry is a negative - Please enter a positive number");
+                txt_input.Clear();
+                txt_input.Focus();
+            }
+            //Validation successful. - Calculate inches to CMs
+            else
+            {
+                lbl_inputLabel.Text = "feet";
+                lbl_outputLabel.Text = "metres";
+                dbl_output = dbl_input * dbl_FEET_PER_METRES;
+                txt_output.Text = dbl_output.ToString();
+                //Make outputs visible
+                lbl_LABEL_EQUALS.Visible = true;
+                txt_output.Visible = true;
+                lbl_outputLabel.Visible = true;
+                lbl_inputLabel.Visible = true;
+            }
+        }
+
+        private void btn_YardsToMetres_Click(object sender, EventArgs e)
+        {
+            const double dbl_YARD_PER_METRES = 0.9144;
+            double dbl_input;
+            double dbl_output;
+
+            //Input validation.
+            if (!double.TryParse(txt_input.Text, out dbl_input))
+            {
+                MessageBox.Show("Your entry is not a number or is empty - Please enter a positive number");
+                txt_input.Clear();
+                txt_input.Focus();
+            }
+            else if (dbl_input < 0)
+            {
+                MessageBox.Show("Your entry is a negative - Please enter a positive number");
+                txt_input.Clear();
+                txt_input.Focus();
+            }
+            //Validation successful. - Calculate inches to CMs
+            else
+            {
+                lbl_inputLabel.Text = "yards";
+                lbl_outputLabel.Text = "metres";
+                dbl_output = dbl_input * dbl_YARD_PER_METRES;
+                txt_output.Text = dbl_output.ToString();
+                //Make outputs visible
+                lbl_LABEL_EQUALS.Visible = true;
+                txt_output.Visible = true;
+                lbl_outputLabel.Visible = true;
+                lbl_inputLabel.Visible = true;
+            }
+        }
+
+        private void btn_MilesToKms_Click(object sender, EventArgs e)
+        {
+            const double dbl_MILES_PER_KILOMETRES = 1.609;
+            double dbl_input;
+            double dbl_output;
+
+            //Input validation.
+            if (!double.TryParse(txt_input.Text, out dbl_input))
+            {
+                MessageBox.Show("Your entry is not a number or is empty - Please enter a positive number");
+                txt_input.Clear();
+                txt_input.Focus();
+            }
+            else if (dbl_input < 0)
+            {
+                MessageBox.Show("Your entry is a negative - Please enter a positive number");
+                txt_input.Clear();
+                txt_input.Focus();
+            }
+            //Validation successful. - Calculate inches to CMs
+            else
+            {
+                lbl_inputLabel.Text = "miles";
+                lbl_outputLabel.Text = "kilometres";
+                dbl_output = dbl_input * dbl_MILES_PER_KILOMETRES;
+                txt_output.Text = dbl_output.ToString();
+                //Make outputs visible
+                lbl_LABEL_EQUALS.Visible = true;
+                txt_output.Visible = true;
+                lbl_outputLabel.Visible = true;
+                lbl_inputLabel.Visible = true;
+            }
+        }
+
+        private void frm_distanceConverter_Load(object sender, EventArgs e)
+        {
+
+        }
+        //button to exit app
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //Event when input is clicked - clears relevant labels and outputs
+        private void txt_input_Click(object sender, EventArgs e)
+        {
+            txt_input.Clear();
+            lbl_LABEL_EQUALS.Visible = false;
+            txt_output.Visible = false;
+            lbl_outputLabel.Visible = false;
+            lbl_inputLabel.Visible = false;
+        }
+        //Event when user enters input - clears relevant labels and outputs
+        private void txt_input_Enter(object sender, EventArgs e)
+        {
+            txt_input.Clear();
+            lbl_LABEL_EQUALS.Visible = false;
+            txt_output.Visible = false;
+            lbl_outputLabel.Visible = false;
+            lbl_inputLabel.Visible = false;
+        }
+
+        private void frm_distanceConverter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void frm_distanceConverter_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+        }
+    }
+}
